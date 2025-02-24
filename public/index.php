a2b36cf27f0f89a24283bb10767a29e77f4bb7c6
Louis-Marie Bouchet <bouchetlouis44@gmail.com>
2025-02-24 15:51:30 +0100

    commit correction

--- public/index.php
+++ public/index.php
@@ -3,12 +3,13 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
 
-require 'vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
 $app->addRoutingMiddleware();
 $app->add(new BasePathMiddleware($app));
 $app->addErrorMiddleware(true, true, true);
